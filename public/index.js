afe89d839856a65a8b888db38e2cd40794245d1c
gauthier-th <mail@gauthier-thomas.dev>
2021-03-01 00:32:28 +0100

    Add url protocol

--- public/index.js
+++ public/index.js
@@ -1,5 +1,5 @@
 function generateURL() {
-	document.getElementById("result").value = window.location.host + "/filter?calendar_url=" + encodeURIComponent(document.getElementById("url").value) + "&regexp=" + encodeURIComponent(document.getElementById("regexp").value);
+	document.getElementById("result").value = window.location.protocol + "://" + window.location.host + "/filter?calendar_url=" + encodeURIComponent(document.getElementById("url").value) + "&regexp=" + encodeURIComponent(document.getElementById("regexp").value);
 }
 
 document.addEventListener('DOMContentLoaded', function() {
